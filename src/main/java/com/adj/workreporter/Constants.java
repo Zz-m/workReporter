823759524d24854b72d7b6b659147d1efbaf643c
Zz-m <adjim234@gmail.comm>
2017-04-21 17:28:46 +0800

    修改地址表示

--- src/main/java/com/adj/workreporter/Constants.java
+++ src/main/java/com/adj/workreporter/Constants.java
@@ -1,16 +1,23 @@
 package com.adj.workreporter;
 
+import java.net.URL;
+
 /**
  * 应用常量
  * Created by dhx on 2017/4/10.
  */
 public class Constants {
+    private static String DIRECTORY = "";
+    static {
+        URL location = Constants.class.getProtectionDomain().getCodeSource().getLocation();
+        DIRECTORY = location.getFile();
+    }
     static final String DATA_DIR_URL = "data/";
-    public static final String DAILY_WORKS_DATA_URL = DATA_DIR_URL + "today.txt";
-    public static final String LOG_URL = DATA_DIR_URL + "logback";
+    public static final String DAILY_WORKS_DATA_URL = DIRECTORY + DATA_DIR_URL + "today.txt";
+    public static final String LOG_URL = DIRECTORY + DATA_DIR_URL + "logback";
 //    public static final String OUTPUT_DIR = "target/out";
     public static final String WEEKLY_REPORT_OUTPUT__FILE_PATH = "F://weekReport.txt";
 
 
-    public final static String DATABASE_URL = "jdbc:sqlite:" + DATA_DIR_URL + "works.db";
+    public final static String DATABASE_URL = "jdbc:sqlite:" + DIRECTORY + DATA_DIR_URL + "works.db";
 }
